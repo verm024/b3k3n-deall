6206bafa98d202f14577cd4019e866a5efb0854b
Muhammad Naufal <45099597+verm024@users.noreply.github.com>
2022-10-25 18:03:20 +0700

    Task / 1 / Setup Eslint and Prettier (#1)

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  globals: {
+    JSX: true,
+  },
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "import"],
+  rules: {
+    indent: ["warn", 2],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
