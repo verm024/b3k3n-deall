e34b5ea2fd6fa4008f5de16c48e130d254790132
Muhammad Naufal <45099597+verm024@users.noreply.github.com>
2022-10-25 18:39:28 +0700

    Task / 2 / Configure Styled Components (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+
+import styled from "styled-components";
+import GlobalStyles from "./globalStyles";
+
+const Test = styled.div<{ color: string }>`
+  width: 100px;
+  height: 100px;
+  background: ${({ color }) => color};
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyles />
+      <Test color="red" />
     </div>
   );
 }
