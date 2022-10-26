d9ed288f3c6c5293f7e761ca2323fe2d95be775b
Muhammad Naufal Arkan Rafii <muhnaufal251@gmail.com>
2022-10-26 16:36:56 +0700

    Setup React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+const queryClient = new QueryClient();
+
 import GlobalStyles from "./utils/globalStyles";
 
 import { BrowserRouter as Router } from "react-router-dom";
@@ -9,24 +13,27 @@ import { Container } from "./components/atom";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <GlobalStyles />
-        <Container
-          display="flex"
-          alignItems="center"
-          justifyContent="space-between"
-          width="95%"
-          margin="auto"
-        >
-          <p>test</p>
-          <p>test</p>
-        </Container>
-        <Container width="80%" margin="auto">
-          <GlobalRoutes />
-        </Container>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="App">
+          <GlobalStyles />
+          <Container
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            width="95%"
+            margin="auto"
+          >
+            <p>test</p>
+            <p>test</p>
+          </Container>
+          <Container width="80%" margin="auto">
+            <GlobalRoutes />
+          </Container>
+        </div>
+      </Router>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
