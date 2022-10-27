3786dc0e5232d0764ad49568d904181e816b873b
Muhammad Naufal <45099597+verm024@users.noreply.github.com>
2022-10-27 12:07:58 +0700

    Task / 10 / Create BookCard Molecule (#12)
    * Create BookCard Molecule
    
    * Add Line Clamp to Text Atom and BookCard Molecule

--- src/hooks/useResponsive.ts
+++ src/hooks/useResponsive.ts
@@ -1,10 +1,16 @@
 import { useMediaQuery } from "react-responsive";
 
 function useResponsive() {
+  // react-responsive breakpoints reference
   const isDesktop = useMediaQuery({ minWidth: 992 });
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 });
   const isMobile = useMediaQuery({ maxWidth: 767 });
-  return { isDesktop, isTablet, isMobile };
+
+  // Bootstrap breakpoints reference for more detailed breakpoints
+  const isLgScreen = useMediaQuery({ minWidth: 992, maxWidth: 1199 });
+  const isXLScreen = useMediaQuery({ minWidth: 1200, maxWidth: 1399 });
+  const isXXLScreen = useMediaQuery({ minWidth: 1400 });
+  return { isDesktop, isTablet, isMobile, isLgScreen, isXLScreen, isXXLScreen };
 }
 
 export default useResponsive;
