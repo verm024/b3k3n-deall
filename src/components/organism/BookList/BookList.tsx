3f70c0ab3f209c64d2b16568645ce894511673bf
Muhammad Naufal <45099597+verm024@users.noreply.github.com>
2022-10-27 15:26:49 +0700

    Task / 11 / Create BookList Organism (#14)
    * Create BookList Organism and Create Its Logic
    
    * Add Click Function When User Clicking Specific Book
    
    * Move Loop Key to React Fragment

---
+++ src/components/organism/BookList/BookList.tsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+
+import { BookProps } from "../../../utils/constants";
+
+import { Container, Spacer } from "../../atom";
+import { BookCard } from "../../molecules";
+
+interface BookListProps {
+  data: BookProps[];
+  onBookClick?: (book: BookProps) => void;
+  col?: number;
+}
+
+const BookList = ({ data = [], onBookClick, col = 4 }: BookListProps) => {
+  const [mappedBook, setMappedBook] = useState<BookProps[][]>([]);
+
+  useEffect(() => {
+    const arr: BookProps[][] = [];
+    let tempArr = [];
+    for (let i = 0; i < data.length; i++) {
+      tempArr.push(data[i]);
+      if (tempArr.length === col || i === data.length - 1) {
+        arr.push(tempArr);
+        tempArr = [];
+      }
+    }
+    setMappedBook(arr);
+  }, [data, col]);
+
+  return (
+    <Container width="100%">
+      {data.length > 0 &&
+        mappedBook.map((row: BookProps[], index: number) => (
+          <React.Fragment key={index}>
+            <Spacer size={24} />
+            <Container
+              display="flex"
+              justifyContent={col === 1 ? "center" : "space-between"}
+              width="100%"
+            >
+              {row.map((col: BookProps) => (
+                <BookCard
+                  title={col.title}
+                  author={col.authors[0]}
+                  imageSrc={col.cover_url}
+                  key={col.id}
+                  onClick={() => {
+                    if (typeof onBookClick !== "undefined") {
+                      onBookClick(col);
+                    }
+                  }}
+                />
+              ))}
+            </Container>
+          </React.Fragment>
+        ))}
+    </Container>
+  );
+};
+
+export default BookList;
---
+++ src/utils/constants.ts
@@ -0,0 +1,13 @@
+export interface BookProps {
+  id: number;
+  title: string;
+  category_id: number;
+  authors: string[];
+  cover_url: string;
+  description: string;
+  sections: {
+    title: string;
+    content: string;
+  }[];
+  audio_length: number;
+}
