ba6179b299fb1b00dd63dcaa551af91a68265b02
Muhammad Naufal Arkan Rafii <muhnaufal251@gmail.com>
2022-10-29 19:57:08 +0700

    Implement Search and Category Name in Book Category Page

--- src/pages/BookCategory/BookCategory.tsx
+++ src/pages/BookCategory/BookCategory.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient, QueryKey } from "react-query";
 import { useParams } from "react-router-dom";
 
 import { useDispatch } from "react-redux";
@@ -26,12 +26,23 @@ import { useResponsive } from "../../hooks";
 
 const BookCategory = () => {
   const { isMobile, isTablet, isLgScreen } = useResponsive();
-  const [inputState, setInputState] = useState<string>("");
+  const [searchKeyword, setSearchKeyword] = useState<string>("");
   const [page, setPage] = useState<number>(1);
   const [modalData, setModalData] = useState<BookProps>();
   const { id: categoryId } = useParams();
+  const [searchResult, setSearchResult] = useState<{
+    isSearching: boolean;
+    results: BookProps[];
+  }>({ isSearching: false, results: [] });
 
   const dispatch = useDispatch();
+  const queryClient = useQueryClient();
+
+  const categoryQueriesData = queryClient.getQueriesData(["category"]);
+  const bookListQueriesData = queryClient.getQueriesData([
+    "book-list",
+    categoryId,
+  ]);
 
   const { data = [] } = useQuery(["book-list", categoryId, page], async () => {
     const res = await fetch(
@@ -43,9 +54,28 @@ const BookCategory = () => {
     return await res.json();
   });
 
-  const handleInputChange = (e: React.ChangeEvent) => {
+  const handleKeywordChange = (e: React.ChangeEvent) => {
     const target = e.target as HTMLInputElement;
-    setInputState(target.value);
+    setSearchKeyword(target.value);
+  };
+
+  const handleSearch = () => {
+    if (searchKeyword.length === 0) {
+      setSearchResult({ isSearching: false, results: [] });
+    } else {
+      const regexPattern = new RegExp(searchKeyword);
+      if (bookListQueriesData.length > 0) {
+        let allMatchedBooks: BookProps[] = [];
+        for (const bookListQueryData of bookListQueriesData) {
+          // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+          const filtered = (bookListQueryData as [QueryKey, any])[1].filter(
+            (e: BookProps) => regexPattern.test(e.title.toLowerCase())
+          );
+          allMatchedBooks = [...allMatchedBooks, ...filtered];
+        }
+        setSearchResult({ isSearching: true, results: allMatchedBooks });
+      }
+    }
   };
 
   const handleChangePage = (newPage: number) => {
@@ -77,23 +107,44 @@ const BookCategory = () => {
         </CustomLink>
         <Spacer size={8} inline />
         <Text textType="h6" bold>
-          Test
+          {categoryQueriesData.length > 0
+            ? (categoryQueriesData[0][1] as { id: number; name: string }[])
+                .filter((e) => e.id === parseInt(categoryId as string))
+                .map((e, index) => (
+                  <React.Fragment key={index}>{e.name}</React.Fragment>
+                ))
+            : `Category ${categoryId}`}
         </Text>
       </Container>
       <Spacer size={8} inline />
       <Input
-        value={inputState}
-        onChange={handleInputChange}
+        value={searchKeyword}
+        onChange={handleKeywordChange}
         placeholder="Search..."
         width="100%"
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleSearch();
+          }
+        }}
       />
-      <BookList
-        col={isMobile ? 1 : isTablet ? 2 : isLgScreen ? 3 : 4}
-        onBookClick={handleBookClick}
-        data={data}
-      />
-      <Spacer size={52} />
-      <Pagination currentPage={page} onChangePage={handleChangePage} />
+      {!searchResult.isSearching ? (
+        <>
+          <BookList
+            col={isMobile ? 1 : isTablet ? 2 : isLgScreen ? 3 : 4}
+            onBookClick={handleBookClick}
+            data={data}
+          />
+          <Spacer size={52} />
+          <Pagination currentPage={page} onChangePage={handleChangePage} />{" "}
+        </>
+      ) : (
+        <BookList
+          col={isMobile ? 1 : isTablet ? 2 : isLgScreen ? 3 : 4}
+          onBookClick={handleBookClick}
+          data={searchResult.results}
+        />
+      )}
       <BookModal
         modalData={modalData}
         handleClose={() => handleCloseBookModal()}
