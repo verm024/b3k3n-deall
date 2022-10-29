444445b6c559bba7cef2535e667eacab7d3a9038
Muhammad Naufal Arkan Rafii <muhnaufal251@gmail.com>
2022-10-29 12:07:22 +0700

    Add Pagination to Bookmark Page

--- src/pages/Bookmark/Bookmark.tsx
+++ src/pages/Bookmark/Bookmark.tsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { Container, Text, Spacer } from "../../components/atom";
+import { Pagination } from "../../components/molecules";
 import { BookList, BookModal } from "../../components/organism";
+
 import { BookProps } from "../../utils/constants";
 import { useResponsive } from "../../hooks";
 import { selectBookmarks } from "../../store/selectors/bookmarkSelector";
+
 import {
   addNewBookmark,
   removeBookmark,
@@ -13,9 +16,12 @@ import {
 
 const Bookmark = () => {
   const { isMobile, isTablet, isLgScreen } = useResponsive();
-  const data = useSelector(selectBookmarks);
   const dispatch = useDispatch();
+  const data = useSelector(selectBookmarks);
+
   const [modalData, setModalData] = useState<BookProps>();
+  const [mappedData, setMappedData] = useState<BookProps[][]>([]);
+  const [page, setPage] = useState<number>(1);
 
   const handleBookClick = (book: BookProps) => {
     setModalData(book);
@@ -33,6 +39,24 @@ const Bookmark = () => {
     dispatch(removeBookmark(modalData));
   };
 
+  const handleChangePage = (newPage: number) => {
+    setPage(newPage);
+  };
+
+  useEffect(() => {
+    const arr: BookProps[][] = [];
+    const itemsPerPage = 10;
+    let tempArr = [];
+    for (let i = 0; i < data.length; i++) {
+      tempArr.push(data[i]);
+      if (tempArr.length === itemsPerPage || i === data.length - 1) {
+        arr.push(tempArr);
+        tempArr = [];
+      }
+    }
+    setMappedData(arr);
+  }, [data]);
+
   return (
     <Container padding="100px 0 80px 0" width="100%">
       <Text textType="h6" bold>
@@ -42,8 +66,10 @@ const Bookmark = () => {
       <BookList
         col={isMobile ? 1 : isTablet ? 2 : isLgScreen ? 3 : 4}
         onBookClick={handleBookClick}
-        data={data}
+        data={mappedData[page - 1] || []}
       />
+      <Spacer size={52} />
+      <Pagination currentPage={page} onChangePage={handleChangePage} />
       <BookModal
         modalData={modalData}
         handleClose={() => handleCloseBookModal()}
